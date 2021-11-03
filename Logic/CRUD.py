fdba18cfc6bf2c3577b0565d40113bb47f853414
oargaclaudia <91536739+oargaclaudia@users.noreply.github.com>
2021-11-03 19:49:05 +0200

    commit

--- Logic/CRUD.py
+++ Logic/CRUD.py
@@ -16,7 +16,8 @@ def adaugaObiect(id, nume, descriere, pret, locatie, lista):
         raise ValueError("Id-ul exista deja! ")
     if pret < 0:
         raise ValueError("Pretul trebuie sa fie pozitiv!")
-    
+    if len(locatie)!=4:
+        raise ValueError("Locatia trb sa aiba 4 caractere")
     obiect=creeazaObiect(id, nume, descriere, pret, locatie)
     return lista + [obiect]
 
@@ -55,6 +56,8 @@ def modificaObiect(id, nume, descriere, pret, locatie, lista):
     :param lista: o lista de obiecte
     :return: lista modificata
     '''
+    if len(locatie)!=4:
+        raise ValueError("Locatia trb sa aiba 4 caractere")
     if pret < 0:
         raise ValueError("Pretul trebuie sa fie pozitiv!")
 
